a48fe48a3912748e57de53ed5d5594d5f551b9a7
Tarun Singh <tarun.singh1120@gmail.com>
2024-05-27 02:50:40 +0530

    readme

--- src/main/java/com/ecommerce/store/configuration/SecurityConfiguration.java
+++ src/main/java/com/ecommerce/store/configuration/SecurityConfiguration.java
@@ -18,6 +18,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
@@ -28,6 +31,7 @@ public class SecurityConfiguration {
     @Autowired
     private UserService userService;
 
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
